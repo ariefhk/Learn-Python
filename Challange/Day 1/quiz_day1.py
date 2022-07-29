8e4d1eeb420506b67a0e371c2b571b0abca84c10
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-07-29 23:46:49 +0700

    new

---
+++ Challange/Day 1/quiz_day1.py
@@ -0,0 +1,36 @@
+from cmath import sqrt
+
+
+def quizNumber(number):
+    print(f'\n=== QUIZ Number {number} ===')
+
+
+quizNumber(1)
+num1 = 3
+num2 = 4
+print(f'addition: {num1 + num2}')
+print(f'subtraction: {num1 - num2}')
+print(f'multiplication: {num1 * num2}')
+print(f'modulus: {num1 % num2}')
+print(f'division: {num1 / num2}')
+print(f'exponesial: {num1 ** num2}')
+print(f'floor division operator: {num1 // num2}')
+
+quizNumber(2)
+print('Arief Rachman Hakim')
+print('Hakim\'s Family')
+print('Indonesia')
+print('I\'am enjoying 30 days of python')
+
+quizNumber(3)
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Arief', 'Python', 'Indonesia']))
+print(type('Arief Rachman Hakim'))
+print(type('Hakim\' Family'))
+print(type('Indonesia'))
+
+quizNumber(4)
+print(sqrt((8-3)**2 + (10-2)**2))
